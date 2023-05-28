111f869a868dce7b6b1c561038c8161d9931c137
Brittany Harding <echo>
2023-05-27 21:24:20 -0400

    app.py request working!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import traceback
 import pandas as pd
 from joblib import load
-from preprocess import PreprocessData
+from preprocess import PreprocessData, DropAndRenameColumns
 from sklearn.base import BaseEstimator, TransformerMixin
 
 # App definition
@@ -19,13 +19,59 @@ def welcome():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     if request.method == 'GET':
-        return "Prediction page. Try inputting your parameters to get a specific prediction."
+        try:
+            gender = request.args.get('gender')
+            married = request.args.get('married')
+            dependents = request.args.get('dependents')
+            education = request.args.get('education')
+            self_employed = request.args.get('self_employed')
+            applicant_income = request.args.get('applicant_income')
+            coapplicant_income = request.args.get('coapplicant_income')
+            loan_amount = request.args.get('loan_amount')
+            loan_amount_term = request.args.get('loan_amount_term')
+            credit_history = request.args.get('credit_history')
+            property_area = request.args.get('property_area')
+            loan_term_category = request.args.get('loan_term_category')
+
+            query = pd.DataFrame({
+                'Gender': [gender],
+                'Married': [married],
+                'Dependents': [dependents],
+                'Education': [education],
+                'Self_Employed': [self_employed],
+                'ApplicantIncome': [applicant_income],
+                'CoapplicantIncome': [coapplicant_income],
+                'LoanAmount': [loan_amount],
+                'Loan_Amount_Term': [loan_amount_term],
+                'Credit_History': [credit_history],
+                'Property_Area': [property_area],
+                'Loan_Term_Category': [loan_term_category]
+            })
+
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            # Print the columns in the query DataFrame
+            print("Query columns:", query.columns)
+
+            prediction = list(model.predict(query))
+
+            return jsonify({
+                "prediction": str(prediction)
+            })
+
+        except:
+            return jsonify({
+                "trace": traceback.format_exc()
+            })
 
     if request.method == 'POST':
         try:
             json_ = request.json
-            query = pd.DataFrame(json_)
+            query = pd.DataFrame(json_, index=[0])
             query = query.reindex(columns=model_columns, fill_value=0)
+
+            # Print the columns in the query DataFrame
+            print("Query columns:", query.columns)
 
             prediction = list(model.predict(query))
 
@@ -40,8 +86,3 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# push this app to git repo and then clone the whole repository on the AWS terminal
-# make new AWS2 instance - install python (sudo app python) - clone repo apt-get
-# On AWS instancw run app.py (then get that instances IP address) send a request http:get request
--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import KNNImputer
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OrdinalEncoder
+
+
 
 class PreprocessData(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -70,3 +83,33 @@ class PreprocessData(BaseEstimator, TransformerMixin):
         X_transformed = X_transformed.drop('Loan_Term_Category', axis=1)
 
         return X_transformed
+
+class DropAndRenameColumns(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.columns_to_drop = ['Loan_ID', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+        self.column_mapping = {
+            'Gender': 'gender',
+            'Married': 'married',
+            'Dependents': 'dependents',
+            'Education': 'education',
+            'Self_Employed': 'self_employed',
+            'LoanAmount': 'loan_amount',
+            'Loan_Amount_Term': 'loan_amount_term',
+            'Credit_History': 'credit_history',
+            'Loan_Status': 'loan_status',
+            'property_area_Rural': 'property_area_rural',
+            'property_area_Semiurban': 'property_area_semiurban',
+            'property_area_Urban': 'property_area_urban',
+            'Loan_Term_Category_Short-term': 'loan_term_short',
+            'Loan_Term_Category_Medium-term': 'loan_term_medium',
+            'Loan_Term_Category_Long-term': 'loan_term_long',
+            'Loan_Status ': 'loan_status'
+        }
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.drop(self.columns_to_drop, axis=1)
+        X.rename(columns=self.column_mapping, inplace=True)
+        return X
